9bb0c551d1de5c6006b78d08e750f61e1d067477
Kacper Muryń <kontoy91@gmail.com>
2025-02-12 16:33:53 +0100

    Initial commit

---
+++ lib/kantox_cashier.ex
@@ -0,0 +1,18 @@
+defmodule KantoxCashier do
+  @moduledoc """
+  Documentation for `KantoxCashier`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KantoxCashier.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kantox_cashier_test.exs
@@ -0,0 +1,8 @@
+defmodule KantoxCashierTest do
+  use ExUnit.Case
+  doctest KantoxCashier
+
+  test "greets the world" do
+    assert KantoxCashier.hello() == :world
+  end
+end
